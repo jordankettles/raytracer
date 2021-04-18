88122a29c0b9f6481dae4a3990bfd3e4cba0ca10
jordankettles <70495950+jordankettles@users.noreply.github.com>
2021-04-18 15:42:26 +1200

    added plane

--- Plane.cpp
+++ Plane.cpp
@@ -25,9 +25,25 @@ std::vector<RayIntersection> Plane::intersect(const Ray& ray) const {
 
 	std::vector<RayIntersection> result;
 
-	/******************************************************
-	* Code to implement Ray-Plane interesection goes here *
-	******************************************************/
+	Ray inverseRay 	= transform.applyInverse(ray);
+
+	double z0 = inverseRay.point(2);
+	double dz = inverseRay.direction(2);
+	double t = -z0/dz;
+
+	if (std::abs(dz) > epsilon) {
+		RayIntersection hit;
+		hit.point = inverseRay.point + t*inverseRay.direction;
+		if (std::abs(hit.point(0)) <= 1 && std::abs(hit.point(1)) <= 1 && t > 0) {
+			printf("%f\n", t);
+			hit.material = material;
+			hit.normal = Normal(0, 0, 1);
+			hit.point = transform.apply(hit.point);
+			hit.normal = transform.apply(hit.normal);
+			hit.distance = (hit.point - ray.point).norm();
+			result.push_back(hit);
+		}
+	}
 
 	return result;
 }
---
+++ build/CMakeFiles/rayTracer.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Jordan/Documents/GitHub/raytracer/AmbientLightSource.cpp" "CMakeFiles/rayTracer.dir/AmbientLightSource.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/AmbientLightSource.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Camera.cpp" "CMakeFiles/rayTracer.dir/Camera.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Camera.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Colour.cpp" "CMakeFiles/rayTracer.dir/Colour.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Colour.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Cube.cpp" "CMakeFiles/rayTracer.dir/Cube.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Cube.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Cylinder.cpp" "CMakeFiles/rayTracer.dir/Cylinder.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Cylinder.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Direction.cpp" "CMakeFiles/rayTracer.dir/Direction.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Direction.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/DirectionalLightSource.cpp" "CMakeFiles/rayTracer.dir/DirectionalLightSource.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/DirectionalLightSource.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/ImageDisplay.cpp" "CMakeFiles/rayTracer.dir/ImageDisplay.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/ImageDisplay.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/LightSource.cpp" "CMakeFiles/rayTracer.dir/LightSource.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/LightSource.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Material.cpp" "CMakeFiles/rayTracer.dir/Material.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Material.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Matrix.cpp" "CMakeFiles/rayTracer.dir/Matrix.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Matrix.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Normal.cpp" "CMakeFiles/rayTracer.dir/Normal.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Normal.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Object.cpp" "CMakeFiles/rayTracer.dir/Object.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Object.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/PinholeCamera.cpp" "CMakeFiles/rayTracer.dir/PinholeCamera.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/PinholeCamera.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Plane.cpp" "CMakeFiles/rayTracer.dir/Plane.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Plane.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Point.cpp" "CMakeFiles/rayTracer.dir/Point.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Point.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/PointLightSource.cpp" "CMakeFiles/rayTracer.dir/PointLightSource.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/PointLightSource.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Scene.cpp" "CMakeFiles/rayTracer.dir/Scene.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Scene.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/SceneReader.cpp" "CMakeFiles/rayTracer.dir/SceneReader.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/SceneReader.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Sphere.cpp" "CMakeFiles/rayTracer.dir/Sphere.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Sphere.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Transform.cpp" "CMakeFiles/rayTracer.dir/Transform.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Transform.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Tube.cpp" "CMakeFiles/rayTracer.dir/Tube.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Tube.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/Vector.cpp" "CMakeFiles/rayTracer.dir/Vector.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/Vector.cpp.obj.d"
+  "C:/Users/Jordan/Documents/GitHub/raytracer/rayTracerMain.cpp" "CMakeFiles/rayTracer.dir/rayTracerMain.cpp.obj" "gcc" "CMakeFiles/rayTracer.dir/rayTracerMain.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
